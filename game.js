a08fc99d9a8e8e163727661c4587aa6738ce4eb9
Shubh Mehta <shubhmehta@gmail.com>
2016-10-08 01:47:05 +0530

    Basic game ready

--- game.js
+++ game.js
@@ -1,8 +1,14 @@
 
 var correct = function(){
-    $('.bottom').html("<h2>Correct</h2>");
+    $('.bottom').html("<h2>Correct !!!!!!!</h2>");
 };
 
 var wrong = function(){
-     $('.bottom').html("<h2>Wrong</h2>");
+     $('.bottom').html("<h2>Wrong (refresh and try again</h2>");
 };
+
+    $('.olpbtn').on('mouseover',function(){
+    var btnid=$(this).attr('id');
+    $('#'+btnid).html(".");
+    $('#'+btnid).css({'background-color':'transparent'});
+});
