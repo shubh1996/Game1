3bbade1befb8597c5ecb7a127c1c4b87f8739f73
Shubh Mehta <shubhmehta@gmail.com>
2016-10-08 01:55:19 +0530

    yet to improvise the ui

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
 
 var correct = function(){
     $('.bottom').html("<h2>Correct !!!!!!!</h2>");
+    $('#cor').css({'background-color':'green'});
 };
 
 var wrong = function(){
      $('.bottom').html("<h2>Wrong (refresh and try again</h2>");
+     $('#wro').css({'background-color':'red'});
 };
 
     $('.olpbtn').on('mouseover',function(){
